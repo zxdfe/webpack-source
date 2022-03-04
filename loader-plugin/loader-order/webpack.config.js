8faf7ead364334f6183d14744c0c8a5d09dc5ed5
zxd <meetzxd@gmail.com>
2022-03-04 18:07:48 +0800

    update loader-plugin

---
+++ loader-plugin/loader-order/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode:'development',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    path.resolve('./loaders/a-loader'),
+                    path.resolve('./loaders/b-loader')
+                ]
+            }
+        ]
+    }
+}
